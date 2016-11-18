559a10aa84d68af7370fcdfdc0257e5d470a6d2b
Chase Perlen <cperlen@gmail.com>
2016-11-18 18:55:29 -0500

    added comments

--- heat_serial.cc
+++ heat_serial.cc
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
 #include <math.h>
 #include <time.h>
 using namespace std;
@@ -12,39 +12,28 @@ inline double sq(double x){
 
 int main(int argc, char *argv[]){
 
+//start clock
+  double elapsed_t = clock()/float(CLOCKS_PER_SEC);
+  
   if (argc != 2) {
-    printf("USAGE: <nx>\n");
+    printf("USAGE: %s <nx>\n", argv[0]);
     exit(1);
   }
 
-//start clock
-  double elapsed_t = clock()/float(CLOCKS_PER_SEC);
-
   const int nx = atof(argv[1]);
   const double kappa = 1;  //not specified by assignment
+  const double dx = M_PI/nx;
+  const double dt = sq(dx)/(5*kappa); //need numerical stability 
+  const double num_steps = 0.5*sq(M_PI)/(kappa*dt);
 
+//initialize T^n
   double** T_n = new double*[nx];
   for (int i=0; i < nx; i++){
    T_n[i] = new double[nx];
- } 
-
- double** T_n1 = new double*[nx];
- for (int i=0; i < nx; i++){
-   T_n1[i] = new double[nx];
- }
-
-
- double dx = M_PI/nx;
- const double dt = sq(dx)/(5*kappa); //need numerical stability 
- const double num_steps = 0.5*sq(M_PI)/(kappa*dt);
-
-//initialize T^n
-
- for(int i=0; i < nx; i++){
-  for(int j=0; j < nx; j++){ 
+   for(int j=0; j < nx; j++){ 
     T_n[i][j] = 0;
   }
-}
+ } 
 
 //boundary conditions 
 for (int i=0; i < nx; i++){
@@ -52,15 +41,18 @@ for (int i=0; i < nx; i++){
   T_n[i][nx-1] = sq(sin(i * M_PI/double(nx))); //T(x,\pi) = sin^2(x)
 }
 
-//initialize T^{n+1} to capture boundaries
-for(int i=0; i < nx; i++){
-  for(int j=0; j < nx; j++){ 
-    T_n1[i][j]=T_n[i][j];
+//initialize T^n+1
+ double** T_n1 = new double*[nx];
+ for (int i=0; i < nx; i++){
+    T_n1[i] = new double[nx];
+    for(int j=0; j < nx; j++){ 
+      T_n1[i][j] = T_n[i][j];
   }
-}
+ }
 
 //finite difference simulation
-for(int t=0; t < num_steps; t++){
+int t = 0;
+while(t < num_steps){
   for(int i=1; i < nx-1; i++){
     for(int j=1; j < nx-1; j++){ 
       T_n1[i][j] = T_n[i][j] + kappa * dt/sq(dx) * (T_n[i-1][j]+T_n[i+1][j]+T_n[i][j-1]+T_n[i][j+1]-4*T_n[i][j]);
@@ -78,19 +70,8 @@ for(int t=0; t < num_steps; t++){
       T_n[i][j] = T_n1[i][j];
     }
   }
+  t += 1;
 }
-
-double tot = 0.0;
-for(int i=0; i < nx; i++){
-  for(int j=0; j < nx; j++){ 
-    tot += T_n[i][j];
-  }
-}
-
-//time elapsed
-elapsed_t = clock()/float(CLOCKS_PER_SEC) - elapsed_t;
-cout<<"Total time elapsed "<< elapsed_t <<endl;
-cout<<"Average temperature is "<<tot/(sq(nx))<<endl;
 
 //print output
 char filename[50];
@@ -104,6 +85,18 @@ for(int i = 0; i < nx; i++){
   fout<<endl;
 }
 fout.close();
+
+//compute average
+double tot = 0.0;
+for(int i=0; i < nx; i++){
+  for(int j=0; j < nx; j++){ 
+    tot += T_n[i][j];
+  }
+}
+
+elapsed_t = clock()/float(CLOCKS_PER_SEC) - elapsed_t;
+cout<<"Total time elapsed "<< elapsed_t <<endl;
+cout<<"Average temperature is "<<tot/(sq(nx))<<endl;
 
 //free memory
 for(int i = 0; i < nx; i++){
